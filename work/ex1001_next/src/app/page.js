3596171908bf28795ec920de9f3f88063c6d591a
jhg617 <gksruf2233@gmail.com>
2025-10-01 15:12:08 +0900

    Configuring MySQL and Nginx Casting with Docker Compose (Backend)

---
+++ work/ex1001_next/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>쌍용교육센터</h1>
+      <hr/>
+    </div>
+  );
+}
